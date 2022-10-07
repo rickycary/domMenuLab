5453f7628444c89a7db08d65d1147328b1c78c89
Richard <ricky.cary1@gmail.com>
2022-10-07 12:07:22 -0500

    Section 2 completed

--- script.js
+++ script.js
@@ -18,4 +18,56 @@ mainEL.style.backgroundColor = 'var(--main-bg)'
 mainEL.innerHTML = '<h1>SEI Rocks!</h1>'
 
 // Task 1.3
-// Add a class of flex-ctrto mainEl.
+// Add a class of flex-ctr to mainEl.
+mainEL.classList.add("flex-ctr")
+
+
+//=======================
+// 2
+//=======================
+
+
+// Task 2.0
+// Select and cache the <nav id="top-menu">element in a variable named topMenuEl.
+
+const topMenuEl = document.querySelector('#top-menu')
+console.log(topMenuEl)
+
+// Task 2.1 =======================================
+// Set the height topMenuEl element to be 100%.
+// Still needs work 
+topMenuEl.style.height = "100%"
+console.log(topMenuEl)
+//=================================================
+
+// Task 2.2
+// Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
+topMenuEl.style.backgroundColor = "var(--sub-menu-bg)"
+
+// Task 2.3
+
+// Add a class of flex-around to topMenuEl.
+topMenuEl.classList.add("flex-around")
+
+
+// Task 3.1
+
+// Iterate over the entire menuLinks array and for each "link" object:
+
+//     Create an <a> element.
+
+
+
+//     On the new element, add an href attribute with its value set to the href property of the "link" object.
+
+
+
+//     Set the new element's content to the value of the text property of the "link" object.
+
+
+
+//     Append the new element to the topMenuEl element.
+
+
+
+
